1b4d998e17183785302b1952a959db455ffbfdd0
Akira <akira.okuno@outlook.com>
2022-10-03 20:41:55 -0300

    Fix cumulative distinct values counting nas as valid ids

--- src/utils/general_utils.py
+++ src/utils/general_utils.py
@@ -1,9 +1,16 @@
 import datetime
 
+
 def cumulative_distinct_values(series):
-    return (~series.duplicated()).cumsum()
+    cumsum = (~series.dropna().duplicated()).cumsum()
+    series.loc[cumsum.index] = cumsum
+    series = series.fillna(method="ffill")
+    return series
+
 
 def last_day_of_month(d: datetime.date) -> datetime.date:
-    
-        date = datetime.date(d.year + d.month//12, d.month % 12 + 1, 1) - datetime.timedelta(days=1)
-        return datetime.datetime(date.year, date.month, date.day)
+
+    date = datetime.date(
+        d.year + d.month // 12, d.month % 12 + 1, 1
+    ) - datetime.timedelta(days=1)
+    return datetime.datetime(date.year, date.month, date.day)
