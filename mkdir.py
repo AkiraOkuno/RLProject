723bbceda01e06196c12fa7cf2d0ecc1e8a00aac
Akira <akira.okuno@outlook.com>
2022-10-04 13:00:55 -0300

    Add script to create base directories

---
+++ mkdir.py
@@ -0,0 +1,6 @@
+import pathlib
+
+pathlib.Path("data").mkdir(parents=True, exist_ok=True)
+pathlib.Path("data/raw").mkdir(parents=True, exist_ok=True)
+
+pathlib.Path("outputs").mkdir(parents=True, exist_ok=True)
