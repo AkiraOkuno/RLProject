6e131b7cec8ccdaa87ffd1a76a27cc117c22d6fd
Akira <akira.okuno@outlook.com>
2022-10-10 18:52:28 -0300

    Add analysis of guardian interactions with varying DA levels

--- src/exploratory/interventions.py
+++ src/exploratory/interventions.py
@@ -34,9 +34,7 @@ def n_distant_interventions(x: pd.Series):
     Function that takes a pd.Series input of timestamps and returns the number of DA interventions with a difference of more than 60s with respect to previous intervention.
     """
 
-    if x.shape[0] == 0:
-        n_distant_interventions = 0
-    elif x.shape[0] == 1:
+    if x.shape[0] == 1:
         n_distant_interventions = 1
     else:
         diff = (x - x.shift())[1:]
@@ -49,12 +47,19 @@ def n_distant_interventions(x: pd.Series):
 
 # import data
 df_interventions = pd.read_csv(DATA_PATH / "df_interventions.csv")
+df_guardian = pd.read_csv(DATA_PATH / "df_guardian.csv")
+
+# preprocessing
+df_guardian["sent_time"] = pd.to_datetime(df_guardian["sent_time"])
+df_guardian["sent_day"] = df_guardian["sent_time"].dt.strftime("%Y-%m-%d")
 
 df_interventions["sent_time"] = pd.to_datetime(df_interventions["sent_time"])
 df_interventions["sent_day"] = df_interventions["sent_time"].dt.strftime("%Y-%m-%d")
 
+df_guardian = df_guardian.sort_values(["groups_id", "sent_time"])
 df_interventions = df_interventions.sort_values(["groups_id", "sent_time"])
 
+# work with DA restricted interventions
 dfda = df_interventions[df_interventions["intervention_type"] == "DA"]
 
 dfda = (
@@ -63,6 +68,49 @@ dfda = (
 dfda["sent_day"] = pd.to_datetime(dfda["sent_day"])
 dfda.columns = ["groups_id", "sent_day", "DA_count"]
 
+# fill missing dates in dfda, i.e. dates where there where no DA interventions with 0
+new_dfda = pd.DataFrame()
+
+for gid in dfda["groups_id"].dropna().unique():
+
+    dfg = dfda[dfda["groups_id"] == gid]
+    min_date = dfg["sent_day"].min()
+    max_date = dfg["sent_day"].max()
+    idx = pd.date_range(min_date, max_date)
+    da_count_series = dfg["DA_count"]
+    da_count_series.index = pd.DatetimeIndex(dfg["sent_day"])
+    da_count_series = da_count_series.reindex(idx, fill_value=0)
+
+    dfg = pd.DataFrame(da_count_series)
+    dfg["groups_id"] = gid
+    dfg = dfg.reset_index().rename(columns={"index": "sent_day"})
+
+    new_dfda = pd.concat([new_dfda, dfg])
+
+dfda = new_dfda.copy()
+del new_dfda
+
+# check for number of responses in days with different DA levels
+df_guardian_responses = df_guardian.groupby(["groups_id", "sent_day"])["guardian_id"].nunique().reset_index()
+df_guardian_responses = df_guardian_responses.rename(columns={"guardian_id": "n_guardian_responses"})
+
+dfda["sent_day"] = dfda["sent_day"].astype(str)
+df_guardian_responses["sent_day"] = df_guardian_responses["sent_day"].astype(str)
+
+df_guardian_responses = pd.merge(dfda, df_guardian_responses, how="left")
+df_guardian_responses["n_guardian_responses"] = df_guardian_responses["n_guardian_responses"].fillna(0)
+
+df_guardian_responses["n_guardian_responses"] = df_guardian_responses["n_guardian_responses"].astype(int)
+
+# calculate mean responses in each group for each number of daily DA interventions
+df_guardian_responses = (
+    df_guardian_responses.groupby(["groups_id", "DA_count"])["n_guardian_responses"].mean().reset_index()
+)
+
+# calculate mean response across groups for each number of daily DA interventions
+print(df_guardian_responses.groupby("DA_count")["n_guardian_responses"].mean())
+
+######################################################################################################################
 
 for gid in random.sample(list(dfda["groups_id"].dropna().unique()), args.random_groups):
 
