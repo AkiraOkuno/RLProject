9c13f1e1b76683d8264e32584adea163a0da85cc
Akira <akira.okuno@outlook.com>
2022-10-11 21:06:31 -0300

    Add save & read pickle functions to general_utils

--- src/utils/general_utils.py
+++ src/utils/general_utils.py
@@ -1,4 +1,5 @@
 import datetime
+import pickle
 
 
 def cumulative_distinct_values(series):
@@ -12,3 +13,14 @@ def last_day_of_month(d: datetime.date) -> datetime.date:
 
     date = datetime.date(d.year + d.month // 12, d.month % 12 + 1, 1) - datetime.timedelta(days=1)
     return datetime.datetime(date.year, date.month, date.day)
+
+
+def save_pickle(obj, path):
+    with open(path, "wb") as f:
+        pickle.dump(obj, f)
+
+
+def open_pickle(path):
+    with open(path, "rb") as f:
+        output = pickle.load(f)
+    return output
