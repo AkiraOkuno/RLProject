7400083ffe5f18696b1e3f5897149db00bb9abd1
Akira <akira.okuno@outlook.com>
2022-10-17 16:12:21 -0300

    Prepare data format for regression

--- src/models/activation_regression.py
+++ src/models/activation_regression.py
@@ -65,6 +65,9 @@ dfg["guardian_id_order"] = dfg["guardian_ids"].apply(len)
 dfg["guardian_id_order"] = dfg["guardian_id_order"].astype(int).apply(range)
 dfg = dfg.explode(["guardian_ids", "guardian_id_order"])
 
+# if no one responds at all, assume order = 0
+dfg["guardian_id_order"] = dfg["guardian_id_order"].fillna(0)
+
 # how many messages were sent before guardian first message
 # assume value 0 for all if no message was sent
 dfg["cumulative_messages_before_response"] = dfg.apply(
@@ -74,14 +77,35 @@ dfg["cumulative_messages_before_response"] = dfg.apply(
 # daily feature: number of messages in that day
 dfg["daily_total_messages"] = dfg["guardian_ids_history"].apply(len)
 
+# calculate hour dummies and discretize by 3 hour intervals
+df_hour = pd.get_dummies(dfg["DA_intervention_hours"].apply(pd.Series).stack()).sum(level=0)
+
+for h in range(23):
+    if h not in df_hour.columns:
+        df_hour[h] = 0
+
+for h in range(8):
+    col = f"{3*h}-{3*h+2}"
+    dfg[col] = 0
+    for i in range(3):
+
+        dfg[col] += df_hour[3 * h + i]
+
+        # fill nas with 0, i.e. all hour values are 0 for days without interventions
+        dfg[col] = dfg[col].fillna(0)
+
+    # map non zeroes to 1
+    dfg[col] = dfg[col].astype(bool).astype(int)
+
+
 # guardian level response regression: i.e. outcome is with respect to single guardians, not daily aggregate
 X = []
 y = []
 
 features = ["n_guardians"]
 features.extend(dfg.columns[5:])
-features.remove("Other")
-features.remove(0.0)
+# features.remove("Other")
+features.remove("0-2")
 features.remove("weekday")
 features.remove("DA_intervention_hours")
 
@@ -112,4 +136,40 @@ X = pd.DataFrame(X)
 X.columns = features
 y = np.array(y)
 
-# TODO: do regression
+for col in X.columns[1:9]:
+    X[col] = X[col].astype(int)
+
+intervention_values = X[X.columns[1:9]].sum().values
+top3_interventions = X.columns[1:9][intervention_values.argsort()[-3:][::-1]]
+remove = [col for col in X.columns[1:9] if col not in top3_interventions]
+X = X.drop(columns=remove)
+
+# normalize some columns
+normalize_cols = [
+    "n_guardians",
+    "n_moderator_messages",
+    "n_distinct_moderators",
+    "guardian_id_order",
+    "cumulative_messages_before_response",
+    "daily_total_messages",
+]
+# X = general_utils.normalize(X, normalize_cols)
+
+X["constant"] = 1
+
+# do a regression for each guardian
+
+for guardian in dfg["guardian_ids"].dropna().unique():
+
+    X_guardian = X[X["guardian_id"] == guardian]
+    y_guardian = y[X["guardian_id"] == guardian]
+
+    X_guardian = X_guardian.drop(columns=["date", "guardian_id"])
+
+    X_guardian = general_utils.normalize(X_guardian, normalize_cols)
+
+    # remove columns that sum up to 0
+    X_guardian = X_guardian.loc[:, (X_guardian.sum(axis=0) != 0)]
+
+    breakpoint()
+    # TODO: do regression at guardian level
