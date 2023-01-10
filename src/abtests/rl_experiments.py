4d554a7f7c1fdafdc688b294e2dfe54d76459113
Akira <akira.okuno@outlook.com>
2023-01-09 22:19:04 -0300

    Refactor code syntax

--- src/abtests/rl_experiments.py
+++ src/abtests/rl_experiments.py
@@ -4,14 +4,13 @@ import os
 import pathlib
 import sys
 import warnings
+from datetime import datetime, timedelta
 
 import numpy as np
 import pandas as pd
-from tqdm import tqdm
-from datetime import datetime, timedelta
-from xgboost import XGBRegressor, XGBClassifier
 from econml.dr import LinearDRLearner
-
+from tqdm import tqdm
+from xgboost import XGBClassifier, XGBRegressor
 
 sys.path.append(os.getcwd())
 from src.utils import general_utils
@@ -22,8 +21,8 @@ parser.add_argument(
     "--ab_test_number",
     "-ab",
     default=3,
-    choices=[1,2,3],
-    type = int,
+    choices=[1, 2, 3],
+    type=int,
 )
 
 args = parser.parse_args()
@@ -34,43 +33,43 @@ DATABASE_PATH = pathlib.Path("outputs/databases")
 DATA_PATH = pathlib.Path("data/processed")
 OUTPUT_PATH = pathlib.Path("outputs/models/heterogenous_causal_effects")
 
-ORGS1 = [5,6,7,28,29,36,45,46,62]
-ORGS2 = [5,6,7,28,29,36,45,46,62]
+ORGS1 = [5, 6, 7, 28, 29, 36, 45, 46, 62]
+ORGS2 = [5, 6, 7, 28, 29, 36, 45, 46, 62]
 ORGS3 = [46]
 
 # ab test data
 if args.ab_test_number == 1:
 
-    ab_file = 'Noam_Study_1_May26th_TreatmentTags.csv'
-    orgs = [5,6,7,28,29,36,45,46,62]
+    ab_file = "Noam_Study_1_May26th_TreatmentTags.csv"
+    orgs = [5, 6, 7, 28, 29, 36, 45, 46, 62]
     # date = 26th May 9-11 AM
 
 elif args.ab_test_number == 2:
 
-    ab_file = 'ABTest_2_NoamSheets for uploading - All Arms.csv'
-    orgs = [5,6,7,28,29,36,45,46,62]
+    ab_file = "ABTest_2_NoamSheets for uploading - All Arms.csv"
+    orgs = [5, 6, 7, 28, 29, 36, 45, 46, 62]
     # date = 1st August 4-6 PM
 
 else:
-    ab_file = 'Nashik_Study_Dropped_Off_Parents_Consolidated_Treatment_List.csv'
+    ab_file = "Nashik_Study_Dropped_Off_Parents_Consolidated_Treatment_List.csv"
     orgs = [46]
-    date = datetime(2022, 3, 17, 10) # 17th March 9-10 AM
+    date = datetime(2022, 3, 17, 10)  # 17th March 9-10 AM
     # intervention_column = "sample_tag"
     # control_name = "Control - Sample 4"
 
 ab = pd.read_csv(RAW_PATH / ab_file)
-ab_guardians = set(ab["guardian_id"].dropna().unique()) 
+ab_guardians = set(ab["guardian_id"].dropna().unique())
 
 # intervention data
-#interventions = pd.DataFrame(general_utils.open_json(RAW_PATH / "interventions" / "interventions_29.json"))
+# interventions = pd.DataFrame(general_utils.open_json(RAW_PATH / "interventions" / "interventions_29.json"))
 
 df_orgs = pd.DataFrame()
 
 for org in orgs:
 
     df_org = pd.DataFrame(general_utils.open_json(RAW_PATH / "orgs_data" / f"df_guardian_{org}.json"))
-    
-    df_orgs = pd.concat([df_orgs,df_org])
+
+    df_orgs = pd.concat([df_orgs, df_org])
 
 # preprocess
 df_orgs["sent_time"] = pd.to_datetime(df_orgs["sent_time"])
@@ -79,14 +78,16 @@ df_orgs["sent_time"] = pd.to_datetime(df_orgs["sent_time"])
 df_orgs = df_orgs[df_orgs["guardian_id"].isin(ab_guardians)]
 
 # filter only 2 months after intervention
-df_orgs = df_orgs[(df_orgs["sent_time"] >= date-timedelta(days=60)) & (df_orgs["sent_time"] <= date+timedelta(days=60))]
+df_orgs = df_orgs[
+    (df_orgs["sent_time"] >= date - timedelta(days=60)) & (df_orgs["sent_time"] <= date + timedelta(days=60))
+]
 
 # construct variable that assigns to each user if they received treatments
-intervention_dict = dict(zip(ab["guardian_id"],ab["sample_tag"]))
+intervention_dict = dict(zip(ab["guardian_id"], ab["sample_tag"]))
 df_orgs["intervention"] = df_orgs["guardian_id"].map(intervention_dict)
 
 interventions = list(set(df_orgs["intervention"].unique()) - set(["Control - Sample 4"]))
-dummies =  pd.get_dummies(df_orgs["intervention"]).drop(columns=["Control - Sample 4"])
+dummies = pd.get_dummies(df_orgs["intervention"]).drop(columns=["Control - Sample 4"])
 df_orgs = pd.concat([df_orgs, dummies], axis=1)
 
 
@@ -98,7 +99,7 @@ df_dates = df_org = pd.DataFrame(general_utils.open_json(RAW_PATH / "orgs_data" 
 df_dates = df_dates[df_dates["guardian_id"].isin(ab_guardians)]
 df_dates["date_joined"] = pd.to_datetime(df_dates["date_joined"])
 
-date_joined_dict = dict(zip(df_dates["guardian_id"],df_dates["date_joined"]))
+date_joined_dict = dict(zip(df_dates["guardian_id"], df_dates["date_joined"]))
 df_orgs["date_joined"] = df_orgs["guardian_id"].map(date_joined_dict)
 
 # drop guardians not present in guardian_date_left_all_orgs.json
@@ -107,12 +108,16 @@ df_orgs = df_orgs[~df_orgs["date_joined"].isna()]
 covariates_dict = dict.fromkeys(df_orgs["guardian_id"].unique())
 
 group_len_dict = df_orgs.groupby("groups_id")["guardian_id"].apply(len).to_dict()
-n_messages_by_guardian_dict_before = df_orgs[df_orgs["sent_time"] < date].groupby("guardian_id")["interactions_id"].apply(len).to_dict()
-n_messages_by_guardian_dict_after = df_orgs[df_orgs["sent_time"] >= date].groupby("guardian_id")["interactions_id"].apply(len).to_dict()
+n_messages_by_guardian_dict_before = (
+    df_orgs[df_orgs["sent_time"] < date].groupby("guardian_id")["interactions_id"].apply(len).to_dict()
+)
+n_messages_by_guardian_dict_after = (
+    df_orgs[df_orgs["sent_time"] >= date].groupby("guardian_id")["interactions_id"].apply(len).to_dict()
+)
 
 for guardian in covariates_dict.keys():
 
-    # COVARIATES 
+    # COVARIATES
 
     date_joined = date_joined_dict[guardian]
     n_days_since_joined = (date - date_joined).days
@@ -122,16 +127,16 @@ for guardian in covariates_dict.keys():
     except KeyError:
         n_messages_before = 0
 
-    group = df_orgs[df_orgs["guardian_id"]==guardian]["groups_id"].values[0]
+    group = df_orgs[df_orgs["guardian_id"] == guardian]["groups_id"].values[0]
     group_len = group_len_dict[group]
 
     guardian_dict = {}
-    guardian_dict["n_days_since_joined"] = n_days_since_joined  
+    guardian_dict["n_days_since_joined"] = n_days_since_joined
     guardian_dict["group_len"] = group_len
     guardian_dict["n_messages_before_intervention"] = n_messages_before
 
     # TREATMENT
-    guardian_dict["intervention"] = ab[ab["guardian_id"]==guardian]["sample_tag"].values[0]
+    guardian_dict["intervention"] = ab[ab["guardian_id"] == guardian]["sample_tag"].values[0]
 
     # OUTCOME
     try:
@@ -144,13 +149,13 @@ for guardian in covariates_dict.keys():
 
 df = pd.DataFrame(covariates_dict).T
 
-dummies =  pd.get_dummies(df["intervention"]).drop(columns=["Control - Sample 4"])
+dummies = pd.get_dummies(df["intervention"]).drop(columns=["Control - Sample 4"])
 df = pd.concat([df, dummies], axis=1)
-#df = df.drop(columns=["intervention"])
+# df = df.drop(columns=["intervention"])
 
 # normalize outcome and n_messages_before
-#df["n_messages_before_intervention"] /= 60
-#df["outcome"] /= 60
+# df["n_messages_before_intervention"] /= 60
+# df["outcome"] /= 60
 
 
 # Train EconML model with generic helper models
@@ -161,12 +166,16 @@ model = LinearDRLearner(
 )
 
 # Define estimator inputs
-T_bin = df[["Treatment - Sample 1",  "Treatment - Sample 2",  "Treatment - Sample 3"]]  # multiple interventions, or treatments
+T_bin = df[
+    ["Treatment - Sample 1", "Treatment - Sample 2", "Treatment - Sample 3"]
+]  # multiple interventions, or treatments
 Y = df["outcome"].astype(int)  # amount of product purchased, or outcome
 X = df[["n_days_since_joined", "group_len", "n_messages_before_intervention"]]  # heterogeneity feature
 
 # Transform T to one-dimensional array with consecutive integer encoding
-def treat_map(t): return np.dot(t, np.arange(1,t.shape[0]+1))
+def treat_map(t):
+    return np.dot(t, np.arange(1, t.shape[0] + 1))
+
 
 T = np.apply_along_axis(treat_map, 1, T_bin).astype(int)
 
